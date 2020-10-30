e4e88bf9df84aecb0eca6b2ee587486096f1635f
wangerfan <ivan_sir@163.com>
2020-10-30 23:40:22 +0800

    订单系统环境

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,20 @@
 package com.atguigu.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+
+/**
+ * 使用RabbitMQ
+ * 1.引入amqp场景
+ * 2.配置
+ * 3.开启注解@EnableRabbit
+ */
+@EnableRedisHttpSession
+@EnableRabbit
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallOrderApplication {
